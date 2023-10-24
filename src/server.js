e085c81f8a5bb808bc321fde352a1302a1cc1763
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-10-24 11:45:07 -0500

    basic server with '/' route running successfully

---
+++ src/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.status(200).send("Hello World!");
+});
+
+const start = () => {
+    app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+};
+
+module.exports = {start, app };
