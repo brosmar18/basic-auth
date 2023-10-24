cb92b4a2174c4f86e0c49cb318e21928e369393b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-10-24 18:53:15 -0500

    404 handler added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const { sequelizeDatabase } = require('./models');
+const notFound = require('./handlers/404');
 const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT || 5000;
@@ -16,6 +16,8 @@ app.use(authRouter);
 app.get('/', (req, res, next) => {
     res.status(200).send("Hello World!");
 });
+
+app.use('*', notFound);
 
 const start = () => {
     app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
